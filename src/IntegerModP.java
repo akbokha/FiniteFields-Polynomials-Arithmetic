e32f2b5f0f7e3a33096df4eb532150badc8e7fa5
Bart <b.a.j.v.helvert@student.tue.nl>
2017-10-10 16:14:06 +0200

    Created ADT

--- src/IntegerModP.java
+++ src/IntegerModP.java
@@ -1,13 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
+ * An integer with a modulus
  *
- * @author Abdel K. Bokharouss
+ * @author Bart van Helvert
  */
 public class IntegerModP {
-    
+    /**
+     * The value
+     */
+    private int number;
+
+    /**
+     * The modulus
+     */
+    private int mod;
+
+    /**
+     * Creates a new IntegerModP
+     *
+     * @param number The value
+     * @param mod    The modulus
+     */
+    public IntegerModP(int number, int mod) {
+        this.number = number;
+        this.mod = mod;
+    }
+
+    @Override
+    public String toString() {
+        return number + " mod " + mod;
+    }
+
+    /**
+     * Gets the number of the {@link IntegerModP}
+     *
+     * @return The number of the {@link IntegerModP}
+     */
+    public int getNumber(){
+        return number;
+    }
+
+    /**
+     * Sets the number of the {@link IntegerModP}
+     *
+     * @param number The number of the {@link IntegerModP}
+     */
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    /**
+     * Gets the modulus of the {@link IntegerModP}
+     *
+     * @return The modulus of the {@link IntegerModP}
+     */
+    public int getMod() {
+        return mod;
+    }
+
+    /**
+     * Sets the modulus of the {@link IntegerModP}
+     *
+     * @param mod The modulus of the {@link IntegerModP}
+     */
+    public void setMod(int mod) {
+        this.mod = mod;
+    }
+
+
+
 }
