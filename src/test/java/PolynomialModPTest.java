5b3f79311029685a840d1b5990f03b154dd18ad8
Bart van Helvert <b.a.j.v.helvert@student.tue.nl>
2017-10-11 23:22:46 +0200

    Added test case for negate

---
+++ src/test/java/PolynomialModPTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class PolynomialModPTest {
+    @Test
+    public void negate() throws Exception {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list1Neg = new ArrayList<>();
+        list1.add(1);
+        list1.add(-1);
+        list1.add(5);
+        list1.add(-5);
+        list1.add(9);
+        list1.add(-9);
+        PolynomialModP polyl = new PolynomialModP(list1, 3);
+        PolynomialModP poly1Neg = new PolynomialModP(list1Neg, 3);
+        assertEquals(polyl.negate(), poly1Neg);
+    }
+
+    @Test
+    public void product() throws Exception {
+    }
+
+    @Test
+    public void longDivision() throws Exception {
+    }
+
+}
